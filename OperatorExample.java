50e38c4ad587d1791d3b04d478077e236a0731a5
davane2510 <108930149+davane2510@users.noreply.github.com>
2022-07-15 17:52:17 +0530

    Add files via upload

---
+++ OperatorExample.java
@@ -0,0 +1,11 @@
+class OperatorExample{
+public static void main(String args[]){
+int a=70;
+int b=50;
+System.out.println(a+b); 
+System.out.println(a-b); 
+System.out.println(a*b); 
+System.out.println(a/b); 
+}
+    
+}
