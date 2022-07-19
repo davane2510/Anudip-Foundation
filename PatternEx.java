c181796a7dfdadf141441066903e673d79ea0f0e
davane2510 <108930149+davane2510@users.noreply.github.com>
2022-07-19 18:04:44 +0530

    Add files via upload

---
+++ PatternEx.java
@@ -0,0 +1,24 @@
+class PatternEx
+{
+	public static void main(String[] ar)
+	{
+		for(int row=1;row<=6;row++)	
+		{
+			for(int column=1;column<=row;column++)	
+			{
+				System.out.print("#"+"  ");
+			}
+			System.out.println();
+		}
+		for(int row=6;row>=1;row--)	
+		{
+			for(int column=1;column<=row;column++)	
+			{
+				System.out.print("#"+"  ");
+			}
+			System.out.println();
+		}
+	}
+
+
+}
